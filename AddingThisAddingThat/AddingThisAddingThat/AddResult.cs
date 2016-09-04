578e2f2c04f7e7003ee702271559158ebf5caace
Sankar Kumar Indraganti <sankar.indraganti@gmail.com>
2016-09-05 00:26:54 +0100

    AddingThisAddingThat

---
+++ AddingThisAddingThat/AddingThisAddingThat/AddResult.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AddingThisAddingThat
+{
+    public class AddResult
+    {
+        private byte[] f;
+        private byte[] s;
+        private byte[] result;
+
+        public AddResult(byte[] f, byte[] s, byte[] result)
+        {
+            this.f = f;
+            this.s = s;
+            this.result = result;
+        }
+
+        //Override the Equals method to compare two objects of AddResult type.
+        public override bool Equals(object obj)
+        {
+            var objectToCompare = obj as AddResult;
+
+            //objectToCompare may not be null but just in case.
+            if (objectToCompare != null)
+            {
+                return this.f.SequenceEqual(objectToCompare.f) 
+                        && this.s.SequenceEqual(objectToCompare.s) 
+                        && this.result.SequenceEqual(objectToCompare.result);
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ AddingThisAddingThat/AddingThisAddingThat/AddingThisAddingThatTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using NUnit.Framework;
+using System.Collections;
+
+namespace AddingThisAddingThat
+{
+    [TestFixture]
+    public class AddingThisAddingThatTests
+    {
+        public static IEnumerable Add_Source
+        {
+            get
+            {
+                yield return new TestCaseData(new byte[3] { 1, 1, 1 }, new byte[3] { 1, 1, 1 })
+                                        .Returns(new AddResult(new byte[3] { 1, 1, 1 }, new byte[3] { 1, 1, 1 }, new byte[3] { 2, 2, 2 }));
+                yield return new TestCaseData(new byte[3] { 1, 1, 255 }, new byte[3] { 0, 0, 1})
+                                   .Returns(new AddResult(new byte[3] { 1, 1, 255 }, new byte[3] { 0, 0, 1 }, new byte[3] { 1, 1, 0 }));
+                
+            }
+        }
+
+        [Test]
+        [TestCaseSource("Add_Source")]
+        public AddResult Add_UsingARecursiveAlgorithm_ValuesAreAdded(byte[] f, byte[] s)
+        {
+            // Arrange
+            // Act
+            var result = AddRecursive(f, s);
+
+            // Assert
+            return new AddResult(f, s, result);
+        }
+
+        private byte[] AddRecursive(byte[] f, byte[] s)
+        {
+            var result = new byte[f.Length];
+            this.AddRecursive(f, s, result, 0);
+            return result;
+        }
+
+        private void AddRecursive(byte[] f, byte[] s, byte[] result,int index)
+        {
+            if(index == f.Length)
+            {
+                return;
+            }
+            else
+            {
+                result[index] = (byte)(f[index] + s[index]);
+                AddRecursive(f, s, result, index + 1);
+            }
+        }
+    }
+}
